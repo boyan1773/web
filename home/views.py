bfab2ffe841a422837b5f1411200d85a7423a840
boyan1773 <jajh104310@gmail.com>
2022-10-05 21:42:43 +0800

    name

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return render(request,'home.html')
+
+def news(request):
+    return render(request,'news.html')
+
+def course(request):
+    return render(request,'course.html')
+
+def teachers(request):
+    return render(request,'teachers.html')
+
+def admissions(request):
+    return render(request,'admissions.html')
