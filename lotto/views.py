a50cf51d776f16de5a998d4a8261ca7619586881
boyan1773 <jajh104310@gmail.com>
2022-10-14 01:06:26 +0800

    1014

--- lotto/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import random
-
-
-# Create your views here.
-def lotto (request):
-    number = str(random.randint(1,49))
-    return render(request,'lotto.html',locals())
-
